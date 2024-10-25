bca00ec6af5a31d8c154d2d8b98f758dc03fdf9b
Sonal Tripathi <tripathisonal2004@gmail.com>
2024-10-25 11:51:00 +0530

    Time: 51 ms (86.21%), Space: 29.2 MB (79.31%) - LeetHub

---
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def removeSubfolders(self, folder):
+        """
+        :type folder: List[str]
+        :rtype: List[str]
+        """
+        folder.sort()
+        result=[folder[0]]
+        for i in range(1,len(folder)):
+            last_folder=result[-1]
+            last_folder+="/"
+            if not folder[i].startswith(last_folder):
+                result.append(folder[i])
+        return result
